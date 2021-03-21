1f650d8be2156371fb7d23974a447cbfd8724b42
Pablo <alum.pablo.holgado.barreno@ieslosremedios.org>
2021-03-21 22:27:45 +0100

    Revision Final

--- src/main/java/com/mycompany/piccross/TableroVista.java
+++ src/main/java/com/mycompany/piccross/TableroVista.java
@@ -1,10 +1,14 @@
 package com.mycompany.piccross;
 
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.StageStyle;
 
 public class TableroVista extends GridPane {
 
@@ -14,7 +18,44 @@ public class TableroVista extends GridPane {
 
     public TableroVista(Tablero tablero) {
         this.tablero = tablero;
-        int numero1eny[] = {0,0,0,0,0,0,0,0,0,0};
+        jugar();
+
+        this.mouseController();
+    }
+
+    private void mouseController() {
+        this.setOnMouseClicked((MouseEvent mouseEvent) -> {
+
+            try {
+                System.out.println("\nDetectado clic en X: " + mouseEvent.getX() + ", Y: " + mouseEvent.getY());
+                int colClic = (int) (mouseEvent.getX() / TAM_X);
+                int filClic = (int) (mouseEvent.getY() / TAM_Y);
+                System.out.print("Col: " + colClic + ", Fil: " + filClic);
+                boolean valor = tablero.getJugadorBoolPos(filClic, colClic);
+                System.out.println(" >>> Número: " + valor);
+                if (valor) {
+                    this.setStyle("-fx-grid-lines-visible: true");
+                    Rectangle r = new Rectangle();
+                    r.setWidth(TAM_X);
+                    r.setHeight(TAM_Y);
+                    r.setFill(Color.GREEN);
+                    this.add(r, colClic, filClic);
+                } else {
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setHeaderText(null);
+                    alert.initStyle(StageStyle.UTILITY);
+                    alert.setContentText("Has Perdido");
+                    alert.show();
+                    jugar();
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        });
+    }
+
+    private void jugar() {
+        int numero1eny[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         for (int y = 0; y <= tablero.num_fila; y++) {
             int numero1 = 0;
             if (y == tablero.num_fila) {
@@ -45,18 +86,14 @@ public class TableroVista extends GridPane {
                         this.add(r, x, y);
                         numero1 += tablero.tablero_resuelto1[y][x];
                         numero1eny[x] += tablero.tablero_resuelto1[y][x];
+
                     }
                 }
 
             }
         }
-
-        this.setMaxWidth(TAM_X * tablero.num_colum );
+        this.setMaxWidth(TAM_X * tablero.num_colum);
         this.setMaxHeight(TAM_Y * tablero.num_fila);
-
     }
 
-    public static void EscribrirTableroJugador() {
-
-    }
 }
